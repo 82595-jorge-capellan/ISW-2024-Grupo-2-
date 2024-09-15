f6a747b0cf5c29b12990fa2e95f6bc38a71c1361
Mariano Brarda <64959782+RevelioX@users.noreply.github.com>
2024-09-15 18:13:11 -0300

    Emails funcionando

--- Practico/Trabajos Prácticos Evaluables/TrabajoPractico6/src/services/formularioService.service.js
+++ Practico/Trabajos Prácticos Evaluables/TrabajoPractico6/src/services/formularioService.service.js
@@ -1,5 +1,5 @@
 import axios from "axios";
-
+import emailjs from '@emailjs/browser';
 
 export const ProvinciasGet = async () => {
 
@@ -25,8 +25,25 @@ export const LocalidadesProvincia = async (provincia) => {
 }
 
 export const SendEmail = async (formCont) => {
+    formCont = {
+        destino: 'mecex61246@asaud.com',
+        tipo_pedido: 'Envío Especial',
+        provincia_origen: 'Buenos Aires',
+        localidad_origen: 'Capital Federal',
+        calle_origen: 'Av. Santa Fe',
+        numero_origen: '1234',
+        referencia_origen: 'Ref123',
+        provincia_destino: 'CABA',
+        localidad_destino: 'Villa Devoto',
+        calle_destino: 'Calle Falsa',
+        numero_destino: '5678',
+        referencia_destino: 'Ref456',
+        fecha_retiro: '2024-09-20',
+        fecha_entrega: '2024-09-22'
+    };
     try {
-        await axios.post("localhost:34500", formCont)
+        const result = await emailjs.send("service_qpb1y7m", "template_whr2i5d", formCont, "X6FT9jkeR_LrJ9SeX");
+        console.log(result)
     }catch(error){
         console.log(error)
     }
